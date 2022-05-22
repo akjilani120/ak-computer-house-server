102bee43c87de2b39d0d673ca87114dfef8f673d
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-22 20:20:44 +0600

    added signle data route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -20,6 +20,12 @@ async function run(){
         app.get("/products", async(req , res) =>{
             const result = await productsCollection.find().toArray()
             res.send(result)
+        })
+        app.get('/products/:id', async(req , res) =>{
+          const id = req.params.id;
+          const query = { _id : ObjectId(id)}
+          const result = await productsCollection.findOne(query)
+          res.send(result)
         })
     }finally{
 
