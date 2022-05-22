dce1596ada9d1bad38edf1d7dadb3405398f5742
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-22 11:50:33 +0600

    mongodb data load

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+// Middle ware
+app.use(cors())
+// app.use(express.json)
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.re66n.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  console.log("it is ok")
+    try{
+        await client.connect();
+        const productsCollection = client.db("computer-parts").collection("products");
+        app.get("/products", async(req , res) =>{
+            const result = await productsCollection.find().toArray()
+            res.send(result)
+        })
+    }finally{
+
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Ak computer parts house start')
 })
